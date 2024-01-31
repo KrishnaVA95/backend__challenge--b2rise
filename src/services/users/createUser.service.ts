f8fbe014ca070ab16ec78c0a66e7ea9a522e7bfd
Krishna <krishnavarela.a@gmail.com>
2024-01-31 10:46:22 -0300

    fix: we changed the user return without the password

--- src/services/users/createUser.service.ts
+++ src/services/users/createUser.service.ts
@@ -3,6 +3,7 @@ import { AppDataSource } from "../../data-source";
 import { User } from "../../entities/user.entitie";
 import { TUserRequest, TUserResponse } from "../../interfaces/users.interfaces";
 import { AppError } from "../../errors/AppError";
+import { userSchemaResponse } from "../../schema/users.schema";
 
 
 const createUserService = async(data: TUserRequest): Promise<TUserResponse> =>{
@@ -29,7 +30,7 @@ const createUserService = async(data: TUserRequest): Promise<TUserResponse> =>{
     })
 
     await userRepository.save(user)
-    return user
+    return userSchemaResponse.parse(user)
 }
 
 export {createUserService}
