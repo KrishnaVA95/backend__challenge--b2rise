26b4401c9c3869df29eabb0e7e69f436fa2797b6
Krishna <krishnavarela.a@gmail.com>
2024-01-31 11:10:56 -0300

    feat:we added the listing and update interface for the user and product

--- src/interfaces/products.interfaces.ts
+++ src/interfaces/products.interfaces.ts
@@ -1,8 +1,17 @@
 import { z } from 'zod'
-import { productSchema, productSchemaRequest, productSchemaResponse } from '../schema/products.schema'
+import { listProductsSchemaResponse, productSchema, productSchemaRequest, productSchemaResponse } from '../schema/products.schema'
+import { DeepPartial } from 'typeorm'
 
 type TProduct = z.infer<typeof productSchema>
 type TProductRequest = z.infer<typeof productSchemaRequest>
 type TProductResponse = z.infer<typeof productSchemaResponse>
+type TListProductsResponse = z.infer<typeof listProductsSchemaResponse>
+type TProductUpdate =  DeepPartial<TProductRequest>
 
-export {TProduct, TProductRequest, TProductResponse}
+export {
+    TProduct, 
+    TProductRequest, 
+    TProductResponse,
+    TListProductsResponse,
+    TProductUpdate
+}
