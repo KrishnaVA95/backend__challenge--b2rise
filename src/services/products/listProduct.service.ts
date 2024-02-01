729823354e465320dbc1b3a79ed9fb6a688ef147
Krishna <krishnavarela.a@gmail.com>
2024-02-01 10:29:31 -0300

    feat: Product search with pagination and the possibility of using filters completed

--- src/services/products/listProduct.service.ts
+++ src/services/products/listProduct.service.ts
@@ -1,12 +1,27 @@
 import { AppDataSource } from "../../data-source"
 import { Product } from "../../entities/product.entitie"
-import { TListProductsResponse } from "../../interfaces/products.interfaces"
+import { TListProductsResponse, TProductPagination } from "../../interfaces/products.interfaces"
 import { listProductsSchemaResponse } from "../../schema/products.schema"
 
 
-const listProductsService = async (): Promise<TListProductsResponse> => {
+const listProductsService = async (page: number, perPage: number, filters: any): Promise<TProductPagination> => {
     const productRepository = AppDataSource.getRepository(Product)
-    let products: Product[] = await productRepository.find()
+
+    let products: Product[] | undefined
+
+    if(!page || !perPage){
+        products = await productRepository.find()
+    }else{
+        products = await productRepository.find({
+            where: filters,
+            skip: (page -1) * perPage,
+            take: perPage,
+            order: {
+                title: 'asc',
+            }
+        })
+    }
+
 
     if(products.length !== 0){
         products = products.map(product => ({
@@ -14,8 +29,12 @@ const listProductsService = async (): Promise<TListProductsResponse> => {
             price: Number(product.price)
         }))
     }
-
-    return listProductsSchemaResponse.parse(products)
+    const returnProducts = listProductsSchemaResponse.parse(products)
+    return {
+        page: page || null,
+        perPage: perPage || null,
+        data: returnProducts,
+    }
 }
 
 export { listProductsService }
